6c5ead6490e87734d856e81882477ba59e268b75
JOHN-RONAN S. BEIRA <JOHN-RONAN S. BEIRA@192.168.1.5>
2024-10-16 20:30:32 +0800

    Yahallo Sekai printing

--- OrderApp/src/OrderApp.java
+++ OrderApp/src/OrderApp.java
@@ -14,6 +14,8 @@ public class OrderApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Yahallo Sekai");
     }
     
 }
