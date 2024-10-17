cbd0148dd3c79c27bdb5378c90571c45ba834787
zero <zero@zero>
2024-10-17 09:18:24 +0800

    test

--- OrderApp/src/orderApp/Main.java
+++ OrderApp/src/orderApp/Main.java
@@ -1,13 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-/**
- *
- * @author zero
- */
-
 import orderApp.components.login.LoginPage;
 import orderApp.components.objects.Account;
 public class Main {
--- OrderApp/src/orderApp/components/login/FileRW.java
+++ OrderApp/src/orderApp/components/login/FileRW.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package orderApp.components.login;
-/**
- *
- * @author zero
- */
-
 
 import java.io.*;
 import java.util.*;
+import orderApp.Main;
 public class FileRW {
     
     
